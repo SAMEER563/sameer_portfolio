adc19fefca908a2af0f82b3d3b05e556e35353ba
Sameer Kumar Maurya <sameermaurya482@gmail.com>
2025-11-09 16:42:44 +0530

    some change

--- components/sections/Home2.tsx
+++ components/sections/Home2.tsx
@@ -71,7 +71,7 @@ export default function Home2() {
 										</div>
 										<Link href="assets/resume.pdf" className="btn me-2 text-300 ps-0 mt-4" target="_blank">
 											<i className="ri-download-line text-primary-2" />
-											[ Download my CV ]
+											[ Download My CV ]
 										</Link>
 									</div>
 								</div>

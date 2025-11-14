7318eb0fc94a9244e5e123b287c81e0be6436ce4
Sameer Kumar Maurya <sameermaurya482@gmail.com>
2025-11-14 14:34:29 +0530

    Change in footer

--- components/layout/footer/Footer2.tsx
+++ components/layout/footer/Footer2.tsx
@@ -1,42 +1,51 @@
-
 export default function Footer2() {
-	return (
-		<>
-			<footer>
-				<div className="section-footer-2 position-relative">
-					<div className="container position-relative z-1 border-top border-1 pb-2 pt-4">
-						<div className="text-center">
-							<a className="d-flex main-logo align-items-center justify-content-center mb-3">
-								<img src="assets/imgs/home-page-2/template/favicon.svg" alt="zelio" />
-								<span className="fs-4 ms-2">James.dev</span>
-							</a>
-							<div className="d-flex justify-content-center gap-3">
-								<a href="http://facebook.com">
-									<i className="ri-facebook-circle-fill fs-18" />
-								</a>
-								<a href="http://twitter.com">
-									<i className="ri-twitter-x-fill fs-18" />
-								</a>
-								<a href="http://linkedin.com">
-									<i className="ri-linkedin-fill fs-18" />
-								</a>
-								<a href="http://github.com">
-									<i className="ri-github-fill fs-18" />
-								</a>
-							</div>
-							<div className="navigation d-flex align-items-center justify-content-center flex-wrap gap-4 my-4">
-								<a href="#about" className="fs-6"> About me </a>
-								<a href="#resume" className="fs-6"> Resume </a>
-								<a href="#services" className="fs-6"> Services </a>
-								<a href="#portfolio" className="fs-6"> Portfolio </a>
-								<a href="#blog" className="fs-6"> Blog </a>
-								<a href="#contact" className="fs-6"> Contact </a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</footer>
+    return (
+        <>
+            <footer>
+                <div className="section-footer-2 position-relative">
+                    <div className="container position-relative z-1 border-top border-1 pb-2 pt-4">
+                        <div className="text-center">
+                            <a className="d-flex main-logo align-items-center justify-content-center mb-3">
+                                <img src="assets/imgs/home-page-2/template/favicon.svg" alt="zelio" />
+                                <span className="fs-4 ms-2">Sameer Kumar Maurya</span>
+                            </a>
 
-		</>
-	)
+                            {/* Updated Social Icons */}
+                            <div className="d-flex justify-content-center gap-3">
+                                {/* YouTube replacing Facebook */}
+                                <a href="https://www.youtube.com/@JavascriptTrivia" target="_blank">
+                                    <i className="ri-youtube-fill fs-18" />
+                                </a>
+
+                                {/* Medium replacing Twitter */}
+                                <a href="https://medium.com/@shakyasam563" target="_blank">
+                                    <i className="ri-medium-fill fs-18" />
+                                </a>
+
+                                {/* LinkedIn */}
+                                <a href="https://www.linkedin.com/in/sameer-kumar-maurya/" target="_blank">
+                                    <i className="ri-linkedin-fill fs-18" />
+                                </a>
+
+                                {/* GitHub */}
+                                <a href="https://github.com/SAMEER563" target="_blank">
+                                    <i className="ri-github-fill fs-18" />
+                                </a>
+                            </div>
+
+                            <div className="navigation d-flex align-items-center justify-content-center flex-wrap gap-4 my-4">
+                                <a href="#about" className="fs-6"> About me </a>
+                                <a href="#experience" className="fs-6"> Experiences </a>
+                                <a href="#services" className="fs-6"> Services </a>
+                                <a href="#skills" className="fs-6"> Skills </a>
+                                <a href="#projects" className="fs-6"> Projects </a>
+                                <a href="#contact" className="fs-6"> Contact </a>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </>
+    )
 }
